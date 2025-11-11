9eacd8fc498eaa1a6407a273ddd2c5c499bcb347
anaoliveira01107275-web <ana.oliveira01107275@sesisenaipr.org.br>
2025-11-11 11:47:09 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,143 @@
 import styles from './App.module.css'
-import perfil from '../imagem/perfil.jpg'
+import perfil from '/perfil.jpg'
+import facebook from '/facebook.jpg'
+import instagram from '/instagram.jpg'
+import linkedin from '/linkedin.png'
+import whatsApp from '/whatsapp.webp'
+import HTMLImg from '/html.png'
+import CSSImg from '/css.webp'
+import JSImg from '/js.webp'
+import VITEImg from '/vite.webp'
+import VERCELImg from '/versel.png'
+import REACTImg from '/react.png'
+import foto from '/foto.jpg'
+
+import { useState } from 'react'
 
 function App() {
 
+  const defaultPhoneNumber = '41984965196'
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleChange = (event) => {
+    const{name, value} = event.target
+    setFormData({...formData, [name]: value})
+  }
+
+
+  const handleZap = () => {
+    const {name, email, message} = formData
+
+    const urlZap = `https://api.whatsapp.com/send?phone=${defaultPhoneNumber}&text=
+    Nome:%20${name}%0D%0A
+    Email:%20${email}%0D%0A
+    Mensagem:%20${message}%0D%0A`
+
+    window.open(urlZap, "_blank")
+
+  }
+
+
   return (
     <>
-       <nav className={styles.menu}>
+      <nav className={styles.menu}>
         <a href="#s1">Home</a>
         <a href="#s2">Cards</a>
-        <a href="#s3">Vídeos</a>
+        <a href="#s3">Repositórios</a>
+        <a href="#s5">Onde nasci</a>
         <a href="#s4">Contato</a>
       </nav>
       <main>
-        <section className={styles.s1}id='s1'>
+        <section className={styles.s1} id='s1'>
           <div className={styles.left}>
-            <img className={styles.perfil} src={perfil} alt="Perfil"/>
+            <img className={styles.perfil} src={perfil} alt="Perfil" />
           </div>
           <div className={styles.right}>
             <h2 className={styles.titlle}>Ana Beatriz V. De Oliveira</h2>
-            <p className={styles.paragraph}>Desenvolvedora iniciante, tenho 15 anos sou estudante do 1° ano do ensino médio estou a procura do meu primeiro emprego, sou responsável 
+            <p className={styles.paragraph}>Desenvolvedora iniciante, tenho 15 anos sou estudante do 1° ano do ensino médio estou a procura do meu primeiro emprego, sou responsável
               tenho facilidade de fazer as coisas conforme ordens de alguém não tenho problema com isso se for preciso trabalho em grupo.
             </p>
           </div>
           <h2></h2>
         </section>
 
-        <section id='s2'>
-          <h2>sessao 2</h2>
+        <section className={styles.s2} id='s2'>
+          <h2 className={styles.tecTitle}>Tecnologias</h2>
+          <div className={styles.wrapCards}>
+            <div className={styles.cards}>
+              <img width={80} src={HTMLImg} alt="HMTL" />
+              <h3>HTML</h3>
+              <p>HTML é a sigla para Hypertext Markup Language (Linguagem de Marcação de Hipertexto) e é a linguagem de marcação padrão usada para criar e estruturar páginas da web.</p>
+            </div>
+            <div className={styles.cards}>
+              <img width={80} src={CSSImg} alt="CSS" />
+              <h3>CSS</h3>
+              <p>CSS (Cascading Style Sheets) é uma linguagem de estilo usada para descrever a apresentação visual de um documento escrito em linguagem de marcação como HTML.</p>
+            </div>
+            <div className={styles.cards}>
+              <img width={80} src={JSImg} alt="JS" />
+              <h3>JS</h3>
+              <p>JavaScript é uma linguagem de programação leve, interpretada e versátil, usada para tornar páginas da web dinâmicas e interativas.</p>
+            </div>
+            <div className={styles.cards}>
+              <img width={80} src={VITEImg} alt="VITE" />
+              <h3>VITE</h3>
+              <p>Vite é uma ferramenta de build (compilação) e servidor de desenvolvimento para projetos de front-end, conhecida por sua velocidade e eficiência.</p>
+            </div>
+            <div className={styles.cards}>
+              <img width={80} src={REACTImg} alt="REACT" />
+              <h3>REACT</h3>
+              <p>React é uma biblioteca JavaScript de código aberto usada para construir interfaces de usuário (UI) interativas para sites e aplicativos.</p>
+            </div>
+            <div className={styles.cards}>
+              <img width={80} src={VERCELImg} alt="VERSEL" />
+              <h3>VERSEL</h3>
+              <p>Vercel é uma plataforma de nuvem que facilita o desenvolvimento e a implantação de sites e aplicativos web modernos. </p>
+            </div>
+          </div>
         </section>
+
 
         <section id='s3'>
           <h2>sessao 3</h2>
         </section>
 
-        <section id='s4'>
-          <h2>sessao 4</h2>
+        <section className={styles.s5} id='s5'>
+      
+          <h2>Onde nasci</h2>
+          <p>Nasci em são Paulo na vila prudente (zona leste) mas meus pais vieram para curitiba quando eu tinha apenas 9 message
+            mas eles viveram lá uns 6 anos em São Paulo meu pai era pintor e minha mãe trabalhava em uma farmácia o salário não era muito alto mas eles eram 
+            bons administradores.
+          </p>
+           <img className={styles.foto} src={foto} alt="Vila Prudente" />
+        </section>
+
+        <section id='s4' className={styles.s4}>
+          <h2>CONTATO</h2>
+          <div className={styles.formData}>
+            <label htmlFor="name">Informe seu nome</label>
+            <input type="text" id='name' name='name' value={formData.name} onChange={handleChange} required />
+            <label htmlFor="email">Informe seu email</label>
+            <input type="email" id='email' name='email' value={formData.email} onChange={handleChange} required />
+            <label htmlFor="message">Informe uma mensagem</label>
+            <textarea name="message" id="message" value={formData.message} onChange={handleChange} cols="30 " rows="10" required></textarea>
+            <button onClick={handleZap}>Enviar mensagem</button>
+          </div>
         </section>
       </main>
-  
+
+      <footer className={styles.rodape}>
+        <a href="https://www.facebook.com/" target='_blank' rel='noopener noreferrer'><img width={80} src={facebook} alt="Facebook" /></a>
+        <a href="https://www.instagram.com/" target='_blank' rel='noopener noreferrer'> <img width={70} src={instagram} alt="Instagram" /> </a>
+        <a href="https://www.linkedin.com/" target='_blank' rel='noopener noreferrer'> <img width={70} src={linkedin} alt="Linkedin" /> </a>
+        <a href="https://www.whatsApp.com/" target='_blank' rel='noopener noreferrer'> <img width={80} src={whatsApp} alt="WhatsApp" /> </a>
+      </footer>
     </>
-  ) 
+  )
 }
 export default App
